7da9de798f3d63905ef3a0337243e45b1246b393
Manan patel <160469109+Mananpatel08@users.noreply.github.com>
2025-04-10 10:38:58 +0530

    Delete TifinTrack directory

--- TifinTrack/Tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('wallet-history/' , views.wallet_history, name="wallets" ),
-    path('track-history/' , views.track_history , name="track" )
-]
